5f6fbb7d60076f3a6a2741625af17f8c6407c63e
Theo Sementa <theosementa@gmail.com>
2025-08-10 11:08:24 +0200

    feat: configure fonts

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ function App() {
 
   return (
     <div className='flex flex-col gap-large'>
-      <h1 className='font-bold p-large bg-background-light-200 rounded-extraSmall'>Make Poll</h1>
-      <p>Hello world</p>
+      <p className='text-title-large p-standard bg-background-light-200 rounded-small'>Make Poll</p>
+      <p className='text-body-large'>Hello world</p>
     </div>
   )
 }
