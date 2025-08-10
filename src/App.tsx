dc4f81956218213c1f401339e59e1a4b9ebba2dd
Theo Sementa <theosementa@gmail.com>
2025-08-10 10:34:39 +0200

    feat: init tailwind constants

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import './styles/App.css'
 function App() {
 
   return (
-    <div>
-      <h1 className='font-bold p-4 bg-background_light-200 rounded-3xl'>Make Poll</h1>
+    <div className='flex flex-col gap-large'>
+      <h1 className='font-bold p-large bg-background-light-200 rounded-extraSmall'>Make Poll</h1>
+      <p>Hello world</p>
     </div>
   )
 }
