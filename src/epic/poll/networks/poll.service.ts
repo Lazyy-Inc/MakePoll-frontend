8911f9b9253de2bb43f24bd730293ffd6de98f00
Theo Sementa <theosementa@gmail.com>
2025-08-13 19:37:31 +0200

    feat: pause network

--- src/epic/poll/networks/poll.service.ts
+++ src/epic/poll/networks/poll.service.ts
@@ -1,15 +1,17 @@
 import axiosClient from "../../../network/axios-client";
 import { NetworkPath } from "../../../network/network-path";
+import type { PollModel } from "./models/poll.model";
 
 export class PollService {
-    static async getPoll(pollId: string) {
+    
+    static async getPoll(pollId: string): Promise<PollModel> {
         try {
-            const response = await axiosClient.get(NetworkPath.Poll.getPoll(pollId));
-            return response.data;
+            const response = (await axiosClient.get<PollModel>(NetworkPath.Poll.getPoll(pollId))).data;
+            return response;
         } catch (error) {
             console.error("Error fetching poll:", error);
             throw error;
         }
-
     }
+
 }
