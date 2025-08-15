d2f3ddabb5dd8b29169a52814aea9336fe56a37b
Theo Sementa <theosementa@gmail.com>
2025-08-15 06:25:28 +0200

    feat: make responsive

--- src/epic/settings/screens/settings.screen.tsx
+++ src/epic/settings/screens/settings.screen.tsx
@@ -6,7 +6,7 @@ export const SettingsScreen = () => {
   const [isCaptchaEnabled, setIsCaptchaEnabled] = useState(false)
 
   return (
-    <div className="flex flex-col gap-large w-full h-screen bg-background-light-50 items-start p-large">
+    <div className="flex flex-col gap-large w-full flex-1 bg-background-light-50 items-start p-large">
       <p className="text-title-medium text-background-dark-50">Paramètres du sondage</p>
       <SettingsCheckboxView
         text="Captcha pour accéder au sondage"
